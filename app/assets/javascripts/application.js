d2253b9d6ca9eda8f61b0e9978ddc604c1444002
Zade <brkfstking@outlook.com>
2016-11-29 22:18:38 +0000

    added cart to nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require spree
+//= require spree/frontend/checkout
+//= require spree/frontend/product
+//= require spree/frontend/cart
+
 //= require_tree .
--- vendor/assets/javascripts/spree/frontend/all.js
+++ vendor/assets/javascripts/spree/frontend/all.js
@@ -6,5 +6,11 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require spree/frontend
+
+//= require spree
+//= require spree/frontend/checkout
+//= require spree/frontend/product
+//= require spree/frontend/cart
+
 //= require_tree .
+
